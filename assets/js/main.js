9fef6023dc44579223e95c33e5bf331afea3d8bd
robertnascimento <robertiver13.03@gmail.com>
2023-03-09 00:41:09 -0300

    projeto final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,58 @@
-const pokemonList = document.getElementById('pokemonList')
+const pokemonsList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
+
+
+function orderNumer(pokemon){
+  let numero = pokemon.number
+  if(numero < 10){
+    numeroFormatado = `00${numero}`
+  }else if(numero >= 10 && numero < 100){
+    numeroFormatado = `0${numero}`
+  }else{
+    numeroFormatado = `${numero}`
+  }
+  return numeroFormatado
+}
+
+function loadPokemonItens(limit,offset){
+  
+  pokeApi.getPokemons(offset,limit).then((pokemons = [])  => {
+      const newHtml = pokemons.map((pokemon) => `
+      <li class="pokemon ${pokemon.type}">
+            <span class="number">#${orderNumer(pokemon)}</span>
             <span class="name">${pokemon.name}</span>
-
+  
             <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+              <ol class="types">
+                ${pokemon.types.map((type) => `<li class ="type ${type}">${type}</li>`).join(' ')}
+              </ol>
+              <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
-        </li>
-    `
+          </li>
+          `).join(' ')
+      pokemonsList.innerHTML += newHtml
+  })
+
 }
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
-}
-
-loadPokemonItens(offset, limit)
+loadPokemonItens(limit,offset)
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+  offset += limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+  const qtdRecordNextPage = offset + limit
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
-    }
+  if (qtdRecordNextPage >= maxRecords){
+    const newLimit =  maxRecords - offset;
+    loadPokemonItens(newLimit,offset)
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+  }else{
+    loadPokemonItens(limit,offset)
+  }
+
 })
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,6 +1,7 @@
 
+
 class Pokemon {
-    number;
+    number
     name;
     type;
     types = [];
